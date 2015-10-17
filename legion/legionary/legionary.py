da17100e6e9bfbf584a07347973e3be055bf736e
David <david.sanchez.falero@gmail.com>
2015-10-17 14:57:11 +0200

    there are some bugs, but it should works

--- legion/legionary/legionary.py
+++ legion/legionary/legionary.py
@@ -1,78 +1,37 @@
 from legion.errors import LegionaryException
-from collections import namedtuple
+import time
 
 
-Point = namedtuple('Point', ['x', 'y', 'w'])
+class Point(object):
+    def __init__(self, x=0, y=0, w=0):
+        self.x = x
+        self.y = y
+        self.w = w
+
+
+class Speed(object):
+    def __init__(self):
+        self.x_in = 0
+        self.x = 0
+        self.x_out = 0
+        self.y_in = 0
+        self.y = 0
+        self.y_out = 0
+        self.w_in = 0
+        self.w = 0
+        self.w_out = 0
 
 
 class Legionary(object):
 
-    def __init__(self):
-        self._speed_x = 0
-        self._speed_y = 0
-        self._speed_w = 0
+    def __init__(self, pid_x, pid_y, pid_w):
+        self.speed = Speed
         self.position = Point(0, 0, 0)
         self.destiny = Point(0, 0, 0)
-
-    @property
-    def speed_x(self):
-        return self._speed_x
-
-    @property
-    def speed_y(self):
-        return self._speed_y
-
-    @property
-    def speed_w(self):
-        return self._speed_w
-
-    @speed_x.setter
-    def speed_x(self, speed):
-        self._speed_x = speed
-        self._engines_speed()
-
-    @speed_y.setter
-    def speed_y(self, speed):
-        self._speed_y = speed
-        self._engines_speed()
-
-    @speed_w.setter
-    def speed_w(self, speed):
-        self._speed_w = speed
-        self._engines_speed()
-
-    @property
-    def speeds(self):
-        return [self.speed_x,
-                self.speed_y,
-                self.speed_w]
-
-    @speeds.setter
-    def speeds(self, speeds):
-        if len(speeds) == 3:
-            self._speed_x = speeds[0]
-            self._speed_y = speeds[1]
-            self._speed_w = speeds[2]
-            self._engines_speed()
-
-    def _engines_speed(self):
-        raise LegionaryException()
-
-    @property
-    def real_speed_x(self):
-        raise LegionaryException()
-
-    @property
-    def real_speed_y(self):
-        raise LegionaryException()
-
-    @property
-    def real_speed_w(self):
-        raise LegionaryException()
-
-    @property
-    def real_speed(self):
-        raise LegionaryException()
+        self._pid_x = pid_x
+        self._pid_y = pid_y
+        self._pid_w = pid_w
+        self.last_time = time.time() * 1000
 
     def go_to(self, x, y, w):
         raise LegionaryException()
--- legion/test/legionary/omnidirectional_test.py
+++ legion/test/legionary/omnidirectional_test.py
@@ -6,17 +6,26 @@ from legion.legionary.legionary import Legionary
 class OmnidirectionalTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.testbot = Omnidirectional()
 
     def tearDown(self):
         pass
 
     def simple_test(self):
-        testbot = Omnidirectional()
-        self.assertTrue(isinstance(testbot, Legionary))
-        testbot.speeds = [1, 0, 0]
-        self.assertEqual(testbot.real_speed, [1, 0, 0])
-        testbot.speeds = [0, 1, 0]
-        self.assertEqual(testbot.real_speed, [0, 1, 0])
-        testbot.speeds = [0, 0, 1]
-        self.assertEqual(testbot.real_speed, [0, 0, 1])
+        self.assertTrue(isinstance(self.testbot, Legionary))
+
+    def position_test(self):
+        self._set_speed(1, 0, 0)
+        self._set_speed(0, 1, 0)
+        self._set_speed(1, 1, 0)
+
+    def _set_speed(self, x, y, w):
+        self.testbot.speed.x_in = x
+        self.testbot.speed.y_in = y
+        self.testbot.speed.w_in = w
+        self.testbot.calculate_in_speeds()
+        self.testbot.read_speeds()
+
+        self.assertEqual(self.testbot.speed.x, x)
+        self.assertEqual(self.testbot.speed.y, y)
+        self.assertEqual(self.testbot.speed.w, w)
